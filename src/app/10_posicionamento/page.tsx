90f56c950c257cc6741a01629ba6cef090ed4f38
Tobias Nogueira Lopes <tobias.lopes.712@ufrn.edu.br>
2025-04-25 12:46:18 -0300

    25/04

---
+++ src/app/10_posicionamento/page.tsx
@@ -0,0 +1,50 @@
+export default function Aplicacao(){
+    return (
+        <div className="border p-3">
+
+            {/*<p className="bg-orange-400 m-2 absolute top-2">*/}
+            {/*<p className="bg-orange-400 m-2 fixed bottom-2">*/}            
+            <p className="bg-orange-400 m-2 sticky top-4 z-10">
+                ITEM DE DESTAQUE sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat.
+            </p>
+
+            {/*<p className="bg-purple-400 m-2 relative top-4 left-4 bottom-4 right-4">*/}
+            <p className="bg-purple-400 m-2 sticky top-4 z-20">
+                ITEM 1 sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat. 
+                equi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat.
+            </p>
+
+            
+            <p className="bg-gray-400 m-2 sticky top-4 z-30">
+                ITEM 2 sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat.
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat.
+                Sequi iure illo ad, aspernatur tempora laboriosam cupiditate, 
+                saepe dolor explicabo consequuntur ut soluta minus provident, 
+                nemo numquam similique modi. Quas, placeat.
+            </p>
+
+        </div>
+    )
+}
