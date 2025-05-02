a9fcf74f55a8588d1f8f2fa991be4d4ac67c9a70
Tobias Nogueira Lopes <tobias.lopes.712@ufrn.edu.br>
2025-05-02 13:34:33 -0300

    02/05

---
+++ src/app/11_flexbox/page.tsx
@@ -0,0 +1,21 @@
+export default function Aplicacao(){
+    return (
+        //flex flex-row-reverse
+        //flex flex-col
+        //flex-wrap
+        //basis-64
+        //order-3
+        //grow
+        //justify-
+        //items-
+        //<div className="border-4 p-3 h-screen flex flex-row gap-2 justify-evenly items-center">
+
+        <div className="border-4 p-3 flex gap-2">
+
+            <div className="caixa w-48 grow-0 shrink-0">Caixa 1</div>
+            <div className="caixa w-72 grow shrink">Caixa 2</div>
+            <div className="caixa w-96 grow-0 shrink-0" >Caixa 3</div>
+
+        </div>
+    )
+}
---
+++ src/app/13_container/page.tsx
@@ -0,0 +1,19 @@
+export default function Aplicacao(){
+    return (
+        <div className="container mx-auto bg-green-300 border-3">
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tempore quia consectetur molestias sunt atque, aliquam id tempora nostrum ipsum dignissimos tenetur quo itaque dolore laudantium eos repellat autem nemo!</p>
+
+            <div className="border-4 p-3 grid gap-2 grid-cols-4">
+
+                <div className="caixa">Caixa 1</div>
+                <div className="caixa">Caixa 2</div>
+                <div className="caixa">Caixa 3</div>
+
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo laboriosam molestias nostrum, modi adipisci rem officiis itaque consectetur repellat, neque eum quos quod ipsam animi quam laudantium. Iure, assumenda deleniti!</p>
+
+        </div>
+    )
+}
